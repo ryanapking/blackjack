86d435f3e707087a58639c195be1fb0ecdfa6ba4
Ryan Apking <ryan.apking@gmail.com>
2016-09-09 13:00:51 -0700

    add getBlackjackValue to Card class and calculateScore method to Hand class

--- src/Blackjack.php
+++ src/Blackjack.php
@@ -11,61 +11,103 @@
             $this->player = new Hand("player");
             $this->dealer = new Hand("dealer");
         }
+    }
 
-        class Deck
+    class Deck
+    {
+        public $cards;
+
+        function __construct()
         {
-            private $cards;
+            $this->cards = array();
+            $this->buildDeck();
+            $this->shuffle();
+        }
 
-            function __construct()
-            {
-                $this->cards = array();
-                for ($i = 1; $i < 5; $i++) {
-                    for ($j = 1; $j < 14; $j++) {
-                        $rank = strval($j);
-                        switch ($i) {
-                            case 1:
-                                $suit .= "C";
-                                break;
-                            case 2:
-                                $suit .= "S";
-                                break;
-                            case 3:
-                                $suit .= "H";
-                                break;
-                            case 4:
-                                $suit .= "D";
-                                break;
-                        }
-                        array_push($this->cards, new Card($rank, $suit));
+        function buildDeck()
+        {
+            for ($i = 1; $i < 5; $i++) {
+                for ($j = 1; $j < 14; $j++) {
+                    $rank = strval($j);
+                    switch ($i) {
+                        case 1:
+                            $suit .= "C";
+                            break;
+                        case 2:
+                            $suit .= "S";
+                            break;
+                        case 3:
+                            $suit .= "H";
+                            break;
+                        case 4:
+                            $suit .= "D";
+                            break;
                     }
+                    array_push($this->cards, new Card($rank, $suit));
                 }
             }
         }
 
-        class Hand
+        function shuffle()
         {
-            private $name;
-            private $cards;
-            private $score;
-
-            function __construct($name)
-            {
-                $this->name = $name;
-                $this->score = 0;
-                $this->cards = array();
-            }
+            shuffle($this->cards);
         }
 
-        class Card
+        function pullCard()
         {
-            private $suit;
-            private $rank;
+            return array_pop($this->cards);
+        }
+    }
 
-            function __construct($rank, $suit)
-            {
-                $this->suit = $suit;
-                $this->rank = $rank;
+    class Hand
+    {
+        public $name;
+        public $cards;
+        public $score;
+
+        function __construct($name)
+        {
+            $this->name = $name;
+            $this->score = 0;
+            $this->cards = array();
+        }
+
+        function getCard($card)
+        {
+            array_push($this->cards, $card);
+        }
+
+        function calculateScore()
+        {
+            $this->score = 0;
+            foreach ($this->cards as $card) {
+                $this->score += $card->blackjackValue;
             }
         }
     }
+
+    class Card
+    {
+        public $suit;
+        public $rank;
+        public $blackjackValue;
+
+        function __construct($rank, $suit)
+        {
+            $this->suit = $suit;
+            $this->rank = $rank;
+        }
+
+        function getBlackjackValue()
+        {
+            if ($this->rank == 1) {
+                $this->blackjackValue = 11;
+            } else if ($this->rank < 11) {
+                $this->blackjackValue = $this->rank;
+            } else {
+                $this->blackjackValue = 10;
+            }
+        }
+    }
+
  ?>
